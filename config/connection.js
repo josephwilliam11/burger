d0aae2726b53481dcfeb167d7345f3f42385caed
joe arnold <joearnold@joes-MacBook-Air.local>
2018-03-13 22:31:47 -0600

    joe

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Astros1!',
+    database: 'burger_db'
+  })
+}
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
